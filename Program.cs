9997f18f480dfa79ba4bbd653de91058160e8b53
Zer0ByteGitHub <LanLura1212@protonmail.com>
2023-10-24 04:33:20 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,59 +1,346 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
+//ID: 1
+
+List<string> plade1Række1 = new List<string> { "20", "30", "43", "55", "70" };
+
+List<string> plade1Række2 = new List<string> { "4", "14", "33", "44", "67" };
+
+List<string> plade1Række3 = new List<string> { "29", "49", "68", "78", "87" };
 
 
 
-public class BankoPlade
+
+
+string trukketTal = "0";
+
+int skiftTal;
+
+bool fuldrække1 = false;
+
+bool fuldrække2 = false;
+
+bool fuldPlade = false;
+
+
+
+do
+
 {
-    public Guid PladeID { get; set; }
-    public List<List<int>> Rækker { get; set; }
-    public bool FuldPlade { get; set; }
-}
 
+    // Dette skriver pladerne ud
 
+    for (int i = 0; i < 5; i++)
 
-public class Program
-{
-    public static void Main()
     {
-        // Opret en BankoPlade
-        var bankoPlade = new BankoPlade
-        {
-            PladeID = Guid.NewGuid(),
-            Rækker = new List<List<int>>()
-        };
 
+        Console.Write(plade1Række1[i] + " ");
 
-
-        // Tilføj BankoPlade til dictionary
-        Dictionary<Guid, BankoPlade> bankoPlader = new Dictionary<Guid, BankoPlade>();
-        bankoPlader.Add(bankoPlade.PladeID, bankoPlade);
-
-
-
-        // Erklær pladeID og rækkeNummer
-        Guid pladeID = bankoPlade.PladeID;
-        int rækkeNummer = 0;
-        int tal = 42;
-
-
-
-        // Tjek om pladen er fuld
-        if (bankoPlader.ContainsKey(pladeID))
-        {
-            var plade = bankoPlader[pladeID];
-            if (rækkeNummer < plade.Rækker.Count)
-            {
-                plade.Rækker[rækkeNummer].Remove(tal);
-
-
-
-                if (plade.Rækker.All(række => række.Count == 0))
-                {
-                    plade.FuldPlade = true;
-                }
-            }
-        }
     }
-}
+
+    Console.WriteLine();
+
+    for (int i = 0; i < 5; i++)
+
+    {
+
+        Console.Write(plade1Række2[i] + " ");
+
+    }
+
+    Console.WriteLine();
+
+    for (int i = 0; i < 5; i++)
+
+    {
+
+        Console.Write(plade1Række3[i] + " ");
+
+    }
+
+    Console.WriteLine();
+
+
+
+    //En variabel, som bliver brugt til at ændre i listerne
+
+    trukketTal = Console.ReadLine();
+
+
+
+
+
+    if (plade1Række1.Contains(trukketTal))
+
+    {
+
+        skiftTal = plade1Række1.IndexOf(trukketTal);
+
+        plade1Række1[skiftTal] = "X";
+
+    }
+
+    else if (plade1Række2.Contains(trukketTal))
+
+    {
+
+        skiftTal = plade1Række2.IndexOf(trukketTal);
+
+        plade1Række2[skiftTal] = "X";
+
+    }
+
+    else if (plade1Række3.Contains(trukketTal))
+
+    {
+
+        skiftTal = plade1Række3.IndexOf(trukketTal);
+
+        plade1Række3[skiftTal] = "X";
+
+    }
+
+
+
+    if (plade1Række1.All(x => x == "X") || plade1Række2.All(x => x == "X") || plade1Række3.All(x => x == "X"))
+
+    {
+
+        Console.WriteLine("Du har en fuld række");
+
+        fuldrække1 = true;
+
+        Console.ReadLine();
+
+        Console.WriteLine("Tryk for at fortsætte");
+
+        Console.Clear();
+
+    }
+
+
+
+    else
+
+    {
+
+        Console.Clear();
+
+    }
+
+} while (fuldrække1 == false);
+
+
+
+
+
+
+
+do
+
+{
+
+    for (int i = 0; i < 5; i++)
+
+    {
+
+        Console.Write(plade1Række1[i] + " ");
+
+    }
+
+    Console.WriteLine();
+
+    for (int i = 0; i < 5; i++)
+
+    {
+
+        Console.Write(plade1Række2[i] + " ");
+
+    }
+
+    Console.WriteLine();
+
+    for (int i = 0; i < 5; i++)
+
+    {
+
+        Console.Write(plade1Række3[i] + " ");
+
+    }
+
+    Console.WriteLine();
+
+
+
+    trukketTal = Console.ReadLine();
+
+
+
+    if (plade1Række1.Contains(trukketTal))
+
+    {
+
+        skiftTal = plade1Række1.IndexOf(trukketTal);
+
+        plade1Række1[skiftTal] = "X";
+
+    }
+
+    else if (plade1Række2.Contains(trukketTal))
+
+    {
+
+        skiftTal = plade1Række2.IndexOf(trukketTal);
+
+        plade1Række2[skiftTal] = "X";
+
+    }
+
+    else if (plade1Række3.Contains(trukketTal))
+
+    {
+
+        skiftTal = plade1Række3.IndexOf(trukketTal);
+
+        plade1Række3[skiftTal] = "X";
+
+    }
+
+
+
+    if ((plade1Række1.All(x => x == "X") && plade1Række2.All(x => x == "X")) ||
+
+    (plade1Række2.All(x => x == "X") && plade1Række3.All(x => x == "X")) ||
+
+    (plade1Række3.All(x => x == "X") && plade1Række1.All(x => x == "X")))
+
+    {
+
+        Console.WriteLine("Du har to fulde rækker");
+
+        fuldrække2 = true;
+
+        Console.ReadLine();
+
+        Console.WriteLine("Tryk for at fortsætte");
+
+        Console.Clear();
+
+    }
+
+
+
+
+
+    else
+
+    {
+
+        Console.Clear();
+
+    }
+
+} while (fuldrække2 == false);
+
+
+
+
+
+do
+
+{
+
+    for (int i = 0; i < 5; i++)
+
+    {
+
+        Console.Write(plade1Række1[i] + " ");
+
+    }
+
+    Console.WriteLine();
+
+    for (int i = 0; i < 5; i++)
+
+    {
+
+        Console.Write(plade1Række2[i] + " ");
+
+    }
+
+    Console.WriteLine();
+
+    for (int i = 0; i < 5; i++)
+
+    {
+
+        Console.Write(plade1Række3[i] + " ");
+
+    }
+
+    Console.WriteLine();
+
+
+
+    trukketTal = Console.ReadLine();
+
+
+
+    if (plade1Række1.Contains(trukketTal))
+
+    {
+
+        skiftTal = plade1Række1.IndexOf(trukketTal);
+
+        plade1Række1[skiftTal] = "X";
+
+    }
+
+    else if (plade1Række2.Contains(trukketTal))
+
+    {
+
+        skiftTal = plade1Række2.IndexOf(trukketTal);
+
+        plade1Række2[skiftTal] = "X";
+
+    }
+
+    else if (plade1Række3.Contains(trukketTal))
+
+    {
+
+        skiftTal = plade1Række3.IndexOf(trukketTal);
+
+        plade1Række3[skiftTal] = "X";
+
+    }
+
+
+
+    if (plade1Række1.All(x => x == "X") && plade1Række2.All(x => x == "X") && plade1Række3.All(x => x == "X"))
+
+    {
+
+        Console.WriteLine("Du har fuld plade");
+
+        fuldPlade = true;
+
+        Console.ReadLine();
+
+        Console.WriteLine("Tryk for at fortsætte");
+
+        Console.Clear();
+
+    }
+
+
+
+    else
+
+    {
+
+        Console.Clear();
+
+    }
+
+} while (fuldPlade == false);
